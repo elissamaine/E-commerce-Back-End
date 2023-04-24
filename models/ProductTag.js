ee4d2d74c7a036eca80131186b1824b3a93050c9
elissamaine <emaine97@yahoo.com>
2023-04-24 14:30:55 -0700

    added to .env file and added tables in Category Product ProductTag and Tag models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,28 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // setting up foreign key to product model's id
+      refererences: {
+        model: 'product',
+        key: 'id',
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // setting up foreign key to tag model's id
+      refererences: {
+        model: 'tag',
+        key: 'id',
+      }
+    },
   },
   {
     sequelize,
